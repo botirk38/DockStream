815efc85c481f8cf797644fb25db0cb40f262352
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-05-19 17:17:47 +0100

    Modularize code for file system isolation

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,49 @@
 use anyhow::{Context, Result};
-use std::fs::copy;
+use std::env;
+use std::fs::{self, copy};
+use std::os::unix::fs as unix_fs;
+use std::process::{Command, exit};
 use std::io::Write;
-use std::os::unix::fs;
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    setup_sandbox_environment()?;
 
-    let args: Vec<_> = std::env::args().collect();
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 5 {
+        anyhow::bail!("Not enough arguments provided.");
+    }
     let command = &args[3];
     let command_args = &args[4..];
 
-    std::fs::create_dir_all("/sandbox/usr/local/bin").expect("Failed to create directory");
+    execute_command(command, command_args)
+}
+
+fn setup_sandbox_environment() -> Result<()> {
+    fs::create_dir_all("/sandbox/usr/local/bin")?;
     copy(
         "/usr/local/bin/docker-explorer",
         "/sandbox/usr/local/bin/docker-explorer",
-    )
-    .expect("Failed to copy file");
-    fs::chroot("/sandbox").expect("unable to chroot.");
-    std::env::set_current_dir("/")?;
-    std::fs::create_dir_all("dev/null")?;
+    )?;
+    unix_fs::chroot("/sandbox")?;
+    env::set_current_dir("/")?;
+    fs::create_dir_all("dev/null")?;
 
-    let output = std::process::Command::new(command)
+    Ok(())
+}
+
+fn execute_command(command: &str, command_args: &[String]) -> Result<()> {
+    let output = Command::new(command)
         .args(command_args)
         .output()
-        .with_context(|| {
-            format!(
-                "Tried to run '{}' with arguments {:?}",
-                command, command_args
-            )
-        })?;
+        .with_context(|| format!("Tried to run '{}' with arguments {:?}", command, command_args))?;
 
     std::io::stdout().write_all(&output.stdout)?;
     std::io::stderr().write_all(&output.stderr)?;
 
     if output.status.success() {
-        std::process::exit(0);
+        exit(0);
     } else {
-        std::process::exit(output.status.code().unwrap_or(1));
+        exit(output.status.code().unwrap_or(1));
     }
-
 }
 
