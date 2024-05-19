7bf2df13307d0801254812553dab58fc33193f76
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-05-19 17:08:53 +0100

    Implement file system isolation using chroot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use anyhow::{Context, Result};
+use std::fs::copy;
+use std::io::Write;
+use std::os::unix::fs;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -7,6 +10,17 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+
+    std::fs::create_dir_all("/sandbox/usr/local/bin").expect("Failed to create directory");
+    copy(
+        "/usr/local/bin/docker-explorer",
+        "/sandbox/usr/local/bin/docker-explorer",
+    )
+    .expect("Failed to copy file");
+    fs::chroot("/sandbox").expect("unable to chroot.");
+    std::env::set_current_dir("/")?;
+    std::fs::create_dir_all("dev/null")?;
+
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
@@ -17,14 +31,14 @@ fn main() -> Result<()> {
             )
         })?;
 
+    std::io::stdout().write_all(&output.stdout)?;
+    std::io::stderr().write_all(&output.stderr)?;
+
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        let std_err = std::str::from_utf8(&output.stderr)?;
-        print!("{}", std_out);
-        eprint!("{}", std_err);
+        std::process::exit(0);
     } else {
         std::process::exit(output.status.code().unwrap_or(1));
     }
 
-    Ok(())
 }
+
